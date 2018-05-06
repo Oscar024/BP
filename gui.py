be1a991614ad3a8d2e004905647aa385217c75ae
Oscar024 <or.carvajal.ortiz@gmail.com>
2018-05-06 16:42:21 -0700

    Delete some messages

--- gui.py
+++ gui.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # Form implementation generated from reading ui file './gui.ui'
-#
-# Created by: PyQt4 UI code generator 4.11.4
-#
-# WARNING! All changes made in this file will be lost!
+
 
 from PyQt4 import QtCore, QtGui
 
